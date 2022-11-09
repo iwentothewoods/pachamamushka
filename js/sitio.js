647f5733a51ef49e2f3112711d7a72cd305fa2b4
iwentothewoods <112993603+iwentothewoods@users.noreply.github.com>
2022-11-09 15:18:08 -0300

    big update

--- js/sitio.js
+++ js/sitio.js
@@ -1,30 +1,34 @@
-'use strict'
+var imagenes = ['./img/carrusel1.jpg','./img/carrusel2.jpg','./img/carrusel3.jpg'],
+    cont = 0;
 
-const grande = document.querySelector('.grande')
-const punto = document.querySelectorAll('.punto')
+function carrousel(carrusel){
+    carrusel.addEventListener('click', e =>{
+        let atras = carrusel.querySelector('.atras'),
+            adelante = carrusel.querySelector('.adelante'),
+            img = carrusel.querySelector('img'),
+            tgt = e.target;
 
-//Recorremos todos los puntos
-punto.forEach((cadaPunto, i) => {
-    //Asignamos un click a cadaPunto
-    punto[i].addEventListener('click', () => {
-        //Guardamos la posición de ese punto
-        let posicion = i
-
-        //posicion es 0, transformX es 0
-        //posicion es 1, transformX es -33.33
-        //posicion es 2, transformX es -66.66
-        //op = posición - 33.33
-        let operacion = posicion * -33.33
-
-        //movemos el grande
-        grande.style.transform = `translateX(${ operacion }%)`
-
-        //recorremos los puntos y quitamos el activo a todos
-        punto.forEach((cadaPunto, i) => {
-            punto[i].classList.remove('activo')
-        })
-        //añadimos la clase activo en el punto seleccionado
-        punto[i].classList.add('activo')
-
+            if (tgt == atras) {
+                if (cont > 0) {
+                    img.src = imagenes[cont - 1];
+                    cont --;
+                } else {
+                    img.src = imagenes[imagenes.length - 1];
+                    cont = imagenes.length -1;                 
+                }
+            } else if (tgt == adelante) {
+                if (cont < imagenes.length - 1) {
+                    img.src = imagenes[cont + 1];
+                    cont ++;
+                } else {
+                    img.src = imagenes[0];
+                    cont = 0;                 
+                }
+            }
     })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let carrusel = document.querySelector('.carrusel');
+    carrousel(carrusel);
 });
